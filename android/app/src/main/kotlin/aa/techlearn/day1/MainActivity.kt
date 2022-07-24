44f8685452fdd1f8105457afcef0f607d48afcda
Manjil Koirala <koiralamanjil7@gmail.com>
2022-07-24 11:33:19 +0545

    1st day

---
+++ android/app/src/main/kotlin/aa/techlearn/day1/MainActivity.kt
@@ -0,0 +1,6 @@
+package aa.techlearn.day1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('This is App Bar'),
+        ),
+        body: const Center(child: Text('Hello')),
+      ),
+    );
+  }
+}
